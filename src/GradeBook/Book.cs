48dadfc03ce4eca68a95b278d30fc592fc783ed6
Ehis <ehisjude420@gmail.com>
2021-03-18 01:59:00 +0100

    added a continue jumping statement

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -35,7 +35,7 @@ namespace GradeBook
             {
                 if(grades[counter] == 42.111)
                 {
-                    break;
+                    continue;
                 }
 
                 result.Low = Math.Min(grades[counter], result.Low);
